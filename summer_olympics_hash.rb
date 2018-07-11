3546e3a771bfbfd0ce84e45e8346da4fc86e411f
anjalisebastian <anjalisebastian@icloud.com>
2018-07-11 19:18:58 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -25,12 +25,12 @@ def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
   summer_olympics = {
-    :Atlanta =>"1996",
     :Sydney =>"2000",
     :Athens =>"2004",
     :Beijing =>"2008",
     :London =>"2012"
   }
+  summer_olympics[:Atlanta]="1996"
   summer_olympics.each do |x,y|
     puts "The #{x} summer olympics took place in #{y}."
   end
@@ -40,12 +40,12 @@ def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
   summer_olympics = {
-    :Atlanta =>"1996",
     :Sydney =>"2000",
     :Athens =>"2004",
     :Beijing =>"2008",
     :London =>"2012"
   }
+  summer_olympics[:Atlanta]="1996"
   summer_olympics.each_key do |x, y|
     upcased_cities = x.upcase
     puts upcased_cities
